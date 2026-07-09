10ce3a7ee69494b7afd06b2fbd5242f60dbf1d5a
Vinicius Toshiaki <nakayamavinicius09@gmail.com>
2026-07-08 20:14:51 -0600

    Update install-aide.sh

--- commands/install-aide.sh
+++ commands/install-aide.sh
@@ -1,1 +1,4 @@
+$ sudo apt update 
+$ sudo apt install aide -y
 
+#Shows all the available updates for the system and installs the AIDE package responding yes to any confirmation 'apt' might ask during the installation
