f3488c84b4f2df17877f918becfd94868cdf55f6
Vinicius Toshiaki <nakayamavinicius09@gmail.com>
2026-07-08 20:16:56 -0600

    Delete scripts/install-aide.sh

--- scripts/install-aide.sh
+++
@@ -1,4 +0,0 @@
-$ sudo apt update 
-$ sudo apt install aide -y
-
-#Shows all the available updates for the system and installs the AIDE package responding yes to any confirmation 'apt' might ask during the installation
